2b22c4663436531c16a6e44d02f366e0c792860c
Naoki Kanatani <naokiokane0616@icloud.com>
2021-06-23 10:11:07 +0900

    modal

--- public/js/modal.js
+++ public/js/modal.js
@@ -2,7 +2,7 @@ const modalOpenAction = function(e) {
     const dataModal = e.currentTarget.dataset.modalOpen;
     Array.from(document.querySelectorAll(".modal")).forEach((e,i) => {
         if(e.getAttribute("data-modal") === dataModal) {
-            e.classList.toggle('close');
+            e.classList.toggle('modal-close');
         }
     }) 
 }
@@ -11,22 +11,11 @@ Array.from(document.querySelectorAll('.card-part')).forEach((modalOpenElement) =
     modalOpenElement.addEventListener('click', modalOpenAction);
 });
 
-const modalWrapOpen = function(e) {
-    //クリックしたボタンのdata属性の値を取得
-    const dataModalOpen = e.currentTarget.dataset.modalOpen;
-    //works_modal_wrapper classが付与されている要素を全て取得
-    //取得した全ての要素からdata属性の値が同じ要素を探す
-    //取得した要素へis_open classを付与する
-    Array.from(document.querySelectorAll('.works_modal_wrapper')).forEach((e, i) => {
-      if(e.getAttribute('data-modal') === dataModalOpen){
-        e.classList.toggle('is_open');
-      }
-    })
-  }
+
 
 const CloseModal = function(e) {
     const targetModal = e.currentTarget.closest('.modal');
-    targetModal.classList.toggle('close');
+    targetModal.classList.toggle('modal-close');
 }
 
 Array.from(document.querySelectorAll('.modal-button-part')).forEach((modalCloseElement) => {
